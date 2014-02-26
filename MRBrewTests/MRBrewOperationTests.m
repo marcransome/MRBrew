ef397a34ffb5c66adb570fd9b1012cfeeb01bf1e
Marc Ransome <marc.ransome@fidgetbox.co.uk>
2014-02-26 11:37:25 +0000

    significant test coverage improvements to MRBrewOperationTests

--- MRBrewTests/MRBrewOperationTests.m
+++ MRBrewTests/MRBrewOperationTests.m
@@ -27,6 +27,7 @@
 #import <OCMock/OCMock.h>
 #import "MRBrewOperation.h"
 #import "MRBrewFormula.h"
+#import "MRBrewConstants.h"
 
 @interface MRBrewOperationTests : XCTestCase
 {
@@ -63,6 +64,237 @@
     Class operationClass = [MRBrewOperation class];
     
     XCTAssertTrue([operation isKindOfClass:operationClass], @"Operation should be kind of class %@.", NSStringFromClass(operationClass));
+}
+
+#pragma mark Type Initialisation
+
+- (void)testOperationCreatedWithTypeHasCorrectNameProperty
+{
+    NSArray *params = @[@"param-one", @"param-two"];
+    MRBrewOperation *operation = [MRBrewOperation operationWithType:MRBrewOperationSearch formula:_formula parameters:params];
+    
+    XCTAssertEqual([operation name], MRBrewOperationSearchIdentifier, @"Operation 'name' property should equal the value of the type constant specified in factory method call.");
+}
+
+- (void)testOperationCreatedWithTypeHasCorrectFormulaProperty
+{
+    NSArray *params = @[@"param-one", @"param-two"];
+    MRBrewOperation *operation = [MRBrewOperation operationWithType:MRBrewOperationSearch formula:_formula parameters:params];
+    
+    XCTAssertEqual([operation formula], _formula, @"Operation 'formula' property should equal the formula object specified in factory method call.");
+}
+
+- (void)testOperationCreatedWithTypeHasCorrectParametersProperty
+{
+    NSArray *params = @[@"param-one", @"param-two"];
+    MRBrewOperation *operation = [MRBrewOperation operationWithType:MRBrewOperationSearch formula:_formula parameters:params];
+    
+    XCTAssertEqual([operation parameters], params, @"Operation 'parameters' property should equal the parameters object specified in factory method call.");
+}
+
+#pragma mark Update Operation Initialisation
+
+- (void)testUpdateOperationHasCorrectNameProperty
+{
+    MRBrewOperation *operation = [MRBrewOperation updateOperation];
+    
+    XCTAssertEqual([operation name], MRBrewOperationUpdateIdentifier, @"Operation 'name' property should match value of MRBrewOperationUpdateIdentifier constant.");
+}
+
+- (void)testUpdateOperationHasNilFormulaProperty
+{
+    MRBrewOperation *operation = [MRBrewOperation updateOperation];
+    
+    XCTAssertNil([operation formula], @"Operation 'formula' property should be nil.");
+}
+
+- (void)testUpdateOperationHasNilParametersProperty
+{
+    MRBrewOperation *operation = [MRBrewOperation updateOperation];
+    
+    XCTAssertNil([operation parameters], @"Operation 'parameters' property should be nil.");
+}
+
+#pragma mark List Operation Initialisation
+
+- (void)testListOperationHasCorrectNameProperty
+{
+    MRBrewOperation *operation = [MRBrewOperation listOperation];
+    
+    XCTAssertEqual([operation name], MRBrewOperationListIdentifier, @"Operation 'name' property should match value of MRBrewOperationListIdentifier constant.");
+}
+
+- (void)testListOperationHasNilFormulaProperty
+{
+    MRBrewOperation *operation = [MRBrewOperation listOperation];
+    
+    XCTAssertNil([operation formula], @"Operation 'formula' property should be nil.");
+}
+
+- (void)testListOperationHasNilParametersProperty
+{
+    MRBrewOperation *operation = [MRBrewOperation listOperation];
+    
+    XCTAssertNil([operation parameters], @"Operation 'parameters' property should be nil.");
+}
+
+#pragma mark Search Operation Initialisation
+
+- (void)testSearchOperationHasCorrectNameProperty
+{
+    MRBrewOperation *operation = [MRBrewOperation searchOperation];
+    
+    XCTAssertEqual([operation name], MRBrewOperationSearchIdentifier, @"Operation 'name' property should match value of MRBrewOperationSearchIdentifier constant.");
+}
+
+- (void)testSearchOperationHasNilFormulaProperty
+{
+    MRBrewOperation *operation = [MRBrewOperation searchOperation];
+    
+    XCTAssertNil([operation formula], @"Operation 'formula' property should be nil.");
+}
+
+- (void)testSearchOperationHasNilParametersProperty
+{
+    MRBrewOperation *operation = [MRBrewOperation searchOperation];
+    
+    XCTAssertNil([operation parameters], @"Operation 'parameters' property should be nil.");
+}
+
+- (void)testSearchOperationWithFormulaHasCorrectNameProperty
+{
+    MRBrewOperation *operation = [MRBrewOperation searchOperation:_formula];
+    
+    XCTAssertEqual([operation name], MRBrewOperationSearchIdentifier, @"Operation 'name' property should match value of MRBrewOperationSearchIdentifier constant.");
+}
+
+- (void)testSearchOperationWithFormulaHasCorrectFormulaProperty
+{
+    MRBrewOperation *operation = [MRBrewOperation searchOperation:_formula];
+    
+    XCTAssertEqual([operation formula], _formula, @"Operation 'formula' property should equal the formula object specified in factory method call.");
+}
+
+- (void)testSearchOperationWithFormulaHasNilParametersProperty
+{
+    MRBrewOperation *operation = [MRBrewOperation searchOperation:_formula];
+    
+    XCTAssertNil([operation parameters], @"Operation 'parameters' property should be nil.");
+}
+
+#pragma mark Install Operation Initialisation
+
+- (void)testInstallOperationWithFormulaHasCorrectNameProperty
+{
+    MRBrewOperation *operation = [MRBrewOperation installOperation:_formula];
+    
+    XCTAssertEqual([operation name], MRBrewOperationInstallIdentifier, @"Operation 'name' property should match value of MRBrewOperationInstallIdentifier constant.");
+}
+
+- (void)testInstallOperationWithFormulaHasCorrectFormulaProperty
+{
+    MRBrewOperation *operation = [MRBrewOperation installOperation:_formula];
+    
+    XCTAssertEqual([operation formula], _formula, @"Operation 'formula' property should equal the formula object specified in factory method call.");
+}
+
+- (void)testInstallOperationWithFormulaHasNilParametersProperty
+{
+    MRBrewOperation *operation = [MRBrewOperation installOperation:_formula];
+    
+    XCTAssertNil([operation parameters], @"Operation 'parameters' property should be nil.");
+}
+
+#pragma mark Info Operation Initialisation
+
+- (void)testInfoOperationWithFormulaHasCorrectNameProperty
+{
+    MRBrewOperation *operation = [MRBrewOperation infoOperation:_formula];
+    
+    XCTAssertEqual([operation name], MRBrewOperationInfoIdentifier, @"Operation 'name' property should match value of MRBrewOperationInfoIdentifier constant.");
+}
+
+- (void)testInfoOperationWithFormulaHasCorrectFormulaProperty
+{
+    MRBrewOperation *operation = [MRBrewOperation infoOperation:_formula];
+    
+    XCTAssertEqual([operation formula], _formula, @"Operation 'formula' property should equal the formula object specified in factory method call.");
+}
+
+- (void)testInfoOperationWithFormulaHasNilParametersProperty
+{
+    MRBrewOperation *operation = [MRBrewOperation infoOperation:_formula];
+    
+    XCTAssertNil([operation parameters], @"Operation 'parameters' property should be nil.");
+}
+
+#pragma mark Remove Operation Initialisation
+
+- (void)testRemoveOperationWithFormulaHasCorrectNameProperty
+{
+    MRBrewOperation *operation = [MRBrewOperation removeOperation:_formula];
+    
+    XCTAssertEqual([operation name], MRBrewOperationRemoveIdentifier, @"Operation 'name' property should match value of MRBrewOperationRemoveIdentifier constant.");
+}
+
+- (void)testRemoveOperationWithFormulaHasCorrectFormulaProperty
+{
+    MRBrewOperation *operation = [MRBrewOperation removeOperation:_formula];
+    
+    XCTAssertEqual([operation formula], _formula, @"Operation 'formula' property should equal the formula object specified in factory method call.");
+}
+
+- (void)testRemoveOperationWithFormulaHasNilParametersProperty
+{
+    MRBrewOperation *operation = [MRBrewOperation removeOperation:_formula];
+    
+    XCTAssertNil([operation parameters], @"Operation 'parameters' property should be nil.");
+}
+
+#pragma mark Options Operation Initialisation
+
+- (void)testOptionsOperationWithFormulaHasCorrectNameProperty
+{
+    MRBrewOperation *operation = [MRBrewOperation optionsOperation:_formula];
+    
+    XCTAssertEqual([operation name], MRBrewOperationOptionsIdentifier, @"Operation 'name' property should match value of MRBrewOperationOptionsIdentifier constant.");
+}
+
+- (void)testOptionsOperationWithFormulaHasCorrectFormulaProperty
+{
+    MRBrewOperation *operation = [MRBrewOperation optionsOperation:_formula];
+    
+    XCTAssertEqual([operation formula], _formula, @"Operation 'formula' property should equal the formula object specified in factory method call.");
+}
+
+- (void)testOptionsOperationWithFormulaHasNilParametersProperty
+{
+    MRBrewOperation *operation = [MRBrewOperation optionsOperation:_formula];
+    
+    XCTAssertNil([operation parameters], @"Operation 'parameters' property should be nil.");
+}
+
+#pragma mark Outdated Operation Initialisation
+
+- (void)testOutdatedOperationHasCorrectNameProperty
+{
+    MRBrewOperation *operation = [MRBrewOperation outdatedOperation];
+    
+    XCTAssertEqual([operation name], MRBrewOperationOutdatedIdentifier, @"Operation 'name' property should match value of MRBrewOperationOutdatedIdentifier constant.");
+}
+
+- (void)testOutdatedOperationHasNilFormulaProperty
+{
+    MRBrewOperation *operation = [MRBrewOperation outdatedOperation];
+    
+    XCTAssertNil([operation formula], @"Operation 'formula' property should be nil.");
+}
+
+- (void)testOutdatedOperationHasNilParametersProperty
+{
+    MRBrewOperation *operation = [MRBrewOperation outdatedOperation];
+    
+    XCTAssertNil([operation parameters], @"Operation 'parameters' property should be nil.");
 }
 
 #pragma mark - Equality Tests
@@ -104,12 +336,30 @@
     XCTAssertFalse([operation1 isEqualToOperation:operation2], @"Operations that have a different 'formula' property should not be equal.");
 }
 
+- (void)testEqualityOfOperationsWithDifferentFormulaPropertyWhereOneIsNil
+{
+    MRBrewOperation *operation1 = [MRBrewOperation operationWithName:@"operation-name" formula:_formula parameters:@[@"param-one"]];
+    MRBrewOperation *operation2 = [MRBrewOperation operationWithName:@"operation-name" formula:nil parameters:@[@"param-one"]];
+    
+    XCTAssertFalse([operation1 isEqualToOperation:operation2], @"Operations that have a different 'formula' property should not be equal.");
+}
+
 - (void)testEqualityOfOperationsWithDifferentParametersProperty
 {
     [[[_formula stub] andReturnValue:@YES] isEqualToFormula:[OCMArg any]];
     
     MRBrewOperation *operation1 = [MRBrewOperation operationWithName:@"operation-name" formula:_formula parameters:@[@"param-one"]];
     MRBrewOperation *operation2 = [MRBrewOperation operationWithName:@"operation-name" formula:_formula parameters:@[@"different-param"]];
+    
+    XCTAssertFalse([operation1 isEqualToOperation:operation2], @"Operations that have a different 'parameters' property should not be equal.");
+}
+
+- (void)testEqualityOfOperationsWithDifferentParametersPropertyWhereOneIsNil
+{
+    [[[_formula stub] andReturnValue:@YES] isEqualToFormula:[OCMArg any]];
+    
+    MRBrewOperation *operation1 = [MRBrewOperation operationWithName:@"operation-name" formula:_formula parameters:@[@"param-one"]];
+    MRBrewOperation *operation2 = [MRBrewOperation operationWithName:@"operation-name" formula:_formula parameters:nil];
     
     XCTAssertFalse([operation1 isEqualToOperation:operation2], @"Operations that have a different 'parameters' property should not be equal.");
 }
