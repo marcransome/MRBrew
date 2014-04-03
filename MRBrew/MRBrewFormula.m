91c6d77740666322c6ff3e399c6c8ea1a4311ec7
Marc Ransome <marc.ransome@fidgetbox.co.uk>
2014-04-03 21:34:00 +0100

    removing unnecessary empty string in formula when name parameter set to nil

--- MRBrew/MRBrewFormula.m
+++ MRBrew/MRBrewFormula.m
@@ -47,7 +47,7 @@
 - (instancetype)initWithName:(NSString *)name isNew:(BOOL)isNew isUpdated:(BOOL)isUpdated isInstalled:(BOOL)isInstalled
 {
     if (self = [super init]) {
-        _name = (name == nil ? @"" : [name copy]);  // replace nil with empty string
+        _name = [name copy];
         _isUpdated = isUpdated;
         _isNew = isNew;
         _isInstalled = isInstalled;
